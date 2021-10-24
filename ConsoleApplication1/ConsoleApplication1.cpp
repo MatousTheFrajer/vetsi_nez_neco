fb38887fde7abcdeb2a02c9a071436004f625d21
matou <matou@LAPTOP-2H19UL1S>
2021-10-24 17:05:20 +0200

    Přidejte soubory projektu.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <corecrt_math.h>
+#include <stdlib.h>
+
+int main(void) {
+	int x = 4;
+	int y = 5;
+	printf("Cislo %d ", x);
+	if (x - y == 0) {
+		printf("je rovno cislu %d ", y);
+	}
+	else {
+		if (x > y) {
+			printf("je vetsi nez cislo %d ", y);
+		}
+		else {
+			printf("je mensi nez cislo %d ", y);
+	}
+}
+}
